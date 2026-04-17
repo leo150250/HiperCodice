77139ca600e6585b9d75f79100117e5d075bb10b
Leandro Gabriel - 2045189 <leandro.gabriel@ifro.edu.br>
2026-04-17 11:25:07 -0400

    Diálogos, menu SP

--- public/hiperCodice.js
+++ public/hiperCodice.js
@@ -1,2 +1,45 @@
 const divMenu = document.getElementById("menu");
 const divMenuInicio = document.getElementById("menuInicio");
+const labelConfigSom = document.getElementById("labelConfigSom");
+const labelConfigMusica = document.getElementById("labelConfigMusica");
+const inputConfigSom = document.getElementById("inputConfigSom");
+const inputConfigMusica = document.getElementById("inputConfigMusica");
+
+var menuAberto = null;
+var configSom = true;
+var configMusica = true;
+inputConfigSom.checked = configSom;
+inputConfigMusica.checked = configMusica;
+labelConfigSom.textContent = "Sons: "+(inputConfigSom.checked?"SIM":"NÃO");
+labelConfigMusica.textContent = "Música: "+(inputConfigMusica.checked?"SIM":"NÃO");
+
+function abrirMenu(_menu) {
+	if (menuAberto!=null) {
+		menuAberto.classList.remove("aberto");
+		menuAberto = null;
+	}
+	menuAberto = document.getElementById("menu"+_menu);
+	menuAberto.classList.add("aberto");
+}
+function alternarConfigSom(_valor) {
+	configSom = _valor;
+	labelConfigSom.textContent = "Sons: "+(configSom?"SIM":"NÃO");
+}
+function alternarConfigMusica(_valor) {
+	configMusica = _valor;
+	labelConfigMusica.textContent = "Música: "+(configMusica?"SIM":"NÃO");
+}
+function exibirDialogo(_id) {
+	let dialogo = document.getElementById(_id);
+	dialogo.showModal();
+}
+function esconderDialogo(_id) {
+	let dialogo = document.getElementById(_id);
+	dialogo.close();
+}
+
+//alternarConfigSom(true);
+//alternarConfigMusica(true);
+setTimeout(()=>{
+	abrirMenu("Inicio");
+},500);
