74155643fa1f56098d70b588e5353c7d6cf9b985
Leandro Gabriel - 2045189 <leandro.gabriel@ifro.edu.br>
2026-06-03 14:22:26 -0400

    Ajuste para obter deques aprovados, e preparo para salas personalizadas.

--- public/getDeque.php
+++ public/getDeque.php
@@ -4,7 +4,28 @@ include_once $path.".interno/conexaoBD.php";
 include_once $path.".interno/funcoes.php";
 include_once $path.".interno/estrutura.php";
 
-construirDeque(1,6);
+$idDeque = 0;
+$numAtributos = 0;
+
+if (isset($_POST["deque"])) {
+	$idDeque = (int)$_POST["deque"];
+}
+if (isset($_POST["numAtributos"])) {
+	$idDeque = (int)$_POST["numAtributos"];
+}
+
+if ($idDeque == 0) {
+	$resDeques = BD_query("SELECT id FROM Deques WHERE situacao = 'b' OR situacao = 'c'");
+	//print_r($resDeques);
+	BD_seek($resDeques,rand(0,BD_num_rows($resDeques)-1));
+	$deque = BD_fetch($resDeques);
+	$idDeque = (int)$deque["id"];
+}
+if ($numAtributos == 0) {
+	$numAtributos = 6;
+}
+
+construirDeque($idDeque,$numAtributos);
 $data = $Deque->json();
 
 header('Content-Type: application/json');
