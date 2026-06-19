b74308e0c7711d920abab470464d67330329b6cd
leo150250 <leo.producoes@hotmail.com>
2026-06-19 14:29:26 -0400

    Testes com módulo de filtragem de palavrões

---
+++ server/testePalavrao.php
@@ -0,0 +1,18 @@
+<?php
+$path = "";
+echo "<pre>";
+include $path.".interno/funcoes.php";
+echo "<form method='POST'>";
+echo "<input type='text' name='frase' id='frase' value='' placeholder='Digite uma frase'>";
+echo "<input type='submit' value='Filtrar'>";
+echo "</form>";
+echo "<hr>";
+$frase = "";
+if (isset($_POST["frase"])) {
+	$frase = $_POST["frase"];
+};
+if ($frase != "") {
+	$fraseFiltrada = filtrarString($frase);
+	echo $fraseFiltrada;
+}
+?>
--- shared/.interno/funcoes.php
+++ shared/.interno/funcoes.php
@@ -22,6 +22,122 @@ function registrarBuffer() {
 	ob_start("outBuffer",0,PHP_OUTPUT_HANDLER_STDFLAGS ^ PHP_OUTPUT_HANDLER_REMOVABLE);
 }
 
+$palavrasProibidas = [];
+function carregarPalavrasProibidas() {
+	global $path, $palavrasProibidas;
+	$arquivo = $path.".interno/palavroes.txt";
+	if (file_exists($arquivo)) {
+		$palavrasProibidas = file($arquivo, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+		$palavrasProibidas = array_map('trim', $palavrasProibidas);
+		foreach ($palavrasProibidas as $chave => $palavra) {
+			$novaPalavra = new stdClass();
+			$novaPalavra->regex = substr($palavra, 0, strpos($palavra, " "));
+			$valores = explode(" ",substr($palavra, strpos($palavra, " ")+1));
+			//$novaPalavra->score = intval(substr($palavra, strpos($palavra, " ")+1));
+			$novaPalavra->score = intval($valores[0]);
+			$novaPalavra->offset = isset($valores[1]) ? intval($valores[1]) : 0;
+			$palavrasProibidas[$chave] = $novaPalavra;
+		}
+	} else {
+		$palavrasProibidas = [];
+	}
+	//print_r($palavrasProibidas);
+}
+carregarPalavrasProibidas();
+
+function filtrarString($_string) {
+	global $palavrasProibidas;
+
+	// Converte os números para letras, pra tentar encontrar slangs ocultos
+	$replacedIndices = [];
+	$mapNumeros = [
+		"0" => "o",
+		"1" => "i",
+		"2" => "z",
+		"3" => "e",
+		"4" => "a",
+		"5" => "s",
+		"6" => "g",
+		"7" => "t",
+		"8" => "b",
+		"9" => "g"
+	];
+	$chars = preg_split('//u', $_string, -1, PREG_SPLIT_NO_EMPTY);
+	foreach ($chars as $idx => $char) {
+		if (isset($mapNumeros[$char])) {
+			$chars[$idx] = $mapNumeros[$char];
+			$replacedIndices[$idx] = $char;
+		}
+	}
+	$_string = implode('', $chars);
+
+	$matches = [];
+	// para cada palavra proibida, procura todas as ocorrências
+	foreach ($palavrasProibidas as $palavra) {
+		$pattern = $palavra->regex;
+		if (preg_match_all($pattern, $_string, $m, PREG_OFFSET_CAPTURE)) {
+			foreach ($m[0] as $occ) {
+				$matchText = $occ[0];
+				$byteOffset = $occ[1] + $palavra->offset;
+				// converte offset em número de caracteres (UTF-8)
+				$charStart = mb_strlen(mb_substr($_string, 0, $byteOffset, 'UTF-8'), 'UTF-8');
+				$charLen = mb_strlen($matchText, 'UTF-8');
+				$matches[] = [
+					'start' => $charStart,
+					'len' => $charLen,
+					'score' => $palavra->score
+				];
+			}
+		}
+	}
+	// converte string para array de caracteres UTF-8
+	$chars = preg_split('//u', $_string, -1, PREG_SPLIT_NO_EMPTY);
+	if (!empty($replacedIndices)) {
+		foreach ($replacedIndices as $idx => $origDigit) {
+			$chars[$idx] = $origDigit;
+		}
+	}
+	if (empty($matches)) {
+		return implode('', $chars);
+	}
+
+	$consolidados = [];
+	foreach ($matches as $m) {
+		$start = $m['start'];
+		if (!isset($consolidados[$start])) {
+			$consolidados[$start] = [
+				'start' => $start,
+				'len' => $m['len'],
+				'score' => $m['score'],
+				'_minScore' => $m['score']
+			];
+			continue;
+		}
+		$consolidados[$start]['score'] += $m['score'];
+		if ($m['score'] < $consolidados[$start]['_minScore']) {
+			$consolidados[$start]['_minScore'] = $m['score'];
+			$consolidados[$start]['len'] = $m['len'];
+		}
+	}
+	$matches = array_values($consolidados);
+	unset($consolidados);
+
+	//print_r($matches);
+
+	// aplica máscaras para cada ocorrência cujo score for negativo
+	foreach ($matches as $m) {
+		if ($m['score'] < 0) {
+			$start = $m['start'];
+			$len = $m['len'];
+			for ($i = $start; $i < $start + $len && $i < count($chars); $i++) {
+				$chars[$i] = '♥';
+			}
+		}
+	}
+
+	return implode('', $chars);
+}
+
 $timers = [];
 class Timer {
 	private $funcao;
